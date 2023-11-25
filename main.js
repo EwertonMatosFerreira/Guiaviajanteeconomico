b6a857fd2a82743d10a1914ab51b149326e8e1a7
EwertonMatosFerreira <137197929+EwertonMatosFerreira@users.noreply.github.com>
2023-11-25 02:58:15 -0300

    1.11.1

---
+++ main.js
@@ -0,0 +1,8 @@
+  setTimeout(function() {
+    document.getElementById('buy-button').style.display = 'block';
+  }, 1397000);  // 23 minutos e 17 segundos
+
+  function redirectToPurchase() {
+    // Substitua a URL com a página de compra
+    window.location.href = 'https://pay.kiwify.com.br/9iXdiNs';
+  }
